65cf92c21a1524016da136c4f433b57ddff46deb
Mark Warren Lalata <mw.lalata@jumpdigital.asia>
2018-07-18 16:21:22 +0800

    use apollo server

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ import bodyParser from 'body-parser'
 import chalk from 'chalk'
 import morgan from 'morgan'
 import mongoose from 'mongoose'
-
 import { MONGO_URI } from './config/keys'
+
+import { ApolloServer } from 'apollo-server-express'
+import path from 'path'
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas'
+
+// Model
+import models from './models'
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,14 +19,30 @@ app.use(morgan('combined'))
 // Db definition
 mongoose.connect(MONGO_URI, { useNewUrlParser: true })
 const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
+db.on('error', console.error.bind(console, chalk.red('connection error:')))
 db.once('open', function() {
   // we're connected!
   console.log(chalk.green('MongoDB Connected!!'))
 })
 
+// Route
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
-app.listen(3000, () => console.log(chalk.green('App running at port 3000!')))
+// Apollo Server
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './graphql/schema')))
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './graphql/resolvers')))
+
+const server = new ApolloServer({
+  // These will be defined for both new or existing servers
+  typeDefs,
+  resolvers,
+  // Bind context
+  context: { models }
+  ,
+})
+
+server.applyMiddleware({ app })
+
+app.listen({ port: 3000 }, () => console.log(chalk.green('App running at port 3000!')))
